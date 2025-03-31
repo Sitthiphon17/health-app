c0f188beaf037661e9215947c8edeee2dace0d82
Sitthiphon <sitthiphon17112550@gmail.com>
2025-03-31 20:27:27 +0700

    Deploy to GitHub Pages git status

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h2>ยินดีต้อนรับสู่หน้าแรกของแอป</h2>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home'
+  };
+  </script>
+  
+  <style scoped>
+  /* สไตล์เฉพาะหน้า Home.vue */
+  </style>
+  
