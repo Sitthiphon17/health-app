f86a83a0790b2abed2d110721daa7ec79dfba777
Sitthiphon <sitthiphon17112550@gmail.com>
2025-03-31 21:23:48 +0700

    คำอธิบายสิ่งที่แก้ไข

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/health-app/',  // ชื่อ repository ของคุณ
   plugins: [vue()],
-  server: {
-    host: '0.0.0.0',  // เปิดให้เข้าถึงจาก IP ภายนอก
-    port: 3000,        // กำหนดพอร์ตที่ต้องการ (สามารถเปลี่ยนได้)
-  },
-  base: '/',           // ใช้ '/' เป็น base URL หากไม่ต้องการตั้งชื่อ repository
-  build: {
-    outDir: 'dist',    // กำหนดให้ไฟล์ที่ build ไปยังโฟลเดอร์ 'dist'
-  },
 });
